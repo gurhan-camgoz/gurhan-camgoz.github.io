87e9dd8660ea43cd95c5400c4133264af70a4c51
gurhan-camgoz <135761419+gurhan-camgoz@users.noreply.github.com>
2025-08-14 16:09:03 +0200

    feat: Set up react-router and placeholder pages

---
+++ src/pages/ResearchPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResearchPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Research Page</h1>
+    </div>
+  );
+};
+
+export default ResearchPage;
---
+++ src/pages/TechnicalPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TechnicalPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Technical Page</h1>
+    </div>
+  );
+};
+
+export default TechnicalPage;
